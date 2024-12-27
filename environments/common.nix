0a869bc28771360dced7c2e6bc8a8290010bf27f
shesh-in <6970317@gmail.com>
2024-12-27 15:57:18 +0300

    global rework, not sure if it works

--- environments/common.nix
+++ environments/common.nix
@@ -29,9 +29,9 @@
         };
     };
 
-    programs.git = {
-        enable = true;
-        userName = "shesh-in";
-        userEmail = "6970317@gmail.com";
-    };
+    #programs.git = {
+    #    enable = true;
+    #    userName = "shesh-in";
+    #    userEmail = "6970317@gmail.com";
+    #};
 }
