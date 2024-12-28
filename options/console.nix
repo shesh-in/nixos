5963aedaf419041bede3a0aa51adae8cd31b4806
shesh-in <6970317@gmail.com>
2024-12-28 03:36:01 +0300

    tty font

--- options/console.nix
+++ options/console.nix
@@ -4,7 +4,7 @@
         earlySetup = true;
         #font = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNLNerdFont-Regular.ttf";
         #packages = with pkgs; [ nerd-fonts.jetbrains-mono ];
-        font = "${pkgs.terminus_font}/share/consolefonts/ter-132n.psf.gz";
+        font = "${pkgs.terminus_font}/share/consolefonts/ter-m16b.psf.gz";
         packages = with pkgs; [ terminus_font ];
         keyMap = "us";
     };
