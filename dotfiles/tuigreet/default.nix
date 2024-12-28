84953a04441f1a609efd96f83d38fbe451142d3a
shesh-in <6970317@gmail.com>
2024-12-28 03:19:22 +0300

    tuigreet

--- dotfiles/tuigreet/default.nix
+++ dotfiles/tuigreet/default.nix
@@ -1,27 +1,24 @@
-{ pkgs, inputs, ... }:
-{
-    let 
-        tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
-        hyprland-session = "${pkgs.hyprland-patched}/share/wayland-sessions";
-    in {
-        services.greetd = {
-            enable = true;
-            settings = {
-                default_session = {
-                    command = "${tuigreet} --time --remember --remember-session --sessions ${hyprland-session}";
-                    user = "greeter";
-                };
+{ pkgs, inputs, ... }: let 
+    tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+    hyprland-session = "${pkgs.hyprland}/share/wayland-sessions";
+in {
+    services.greetd = {
+        enable = true;
+        settings = {
+            default_session = {
+                command = "${tuigreet} -g \"Access restricted.\" --time --remember --remember-session --sessions ${hyprland-session} --cmd Hyprland";
+                user = "greeter";
             };
         };
+    };
 
-        systemd.services.greetd.serviceConfig = {
-            Type = "idle";
-            StandardInput = "tty";
-            StandardOutput = "tty";
-            StandardError = "journal";
-            TTYReset = true;
-            TTYVHangup = true;
-            TTYVDisallocate = true;
-        };
+    systemd.services.greetd.serviceConfig = {
+        Type = "idle";
+        StandardInput = "tty";
+        StandardOutput = "tty";
+        StandardError = "journal";
+        TTYReset = true;
+        TTYVHangup = true;
+        TTYVDisallocate = true;
     };
 }
