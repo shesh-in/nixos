65719c6177847aa269d2ae5853c12f1e20fd6126
shesh-in <6970317@gmail.com>
2024-12-28 13:23:08 +0300

    grub theme

--- options/grub2-multiboot.nix
+++ options/grub2-multiboot.nix
@@ -5,36 +5,52 @@
         #os-prober
         efibootmgr
     ];
+    boot = {
+        loader = {
+            systemd-boot.enable = false;
+	        efi = {
+	            canTouchEfiVariables = true;
+    	        efiSysMountPoint = "/boot";
+    	    };
+    	    grub = {
+    	        devices = [ "nodev" ];
+    	        enable = true;
+    	        efiSupport = true;
+    	        useOSProber = false;
+                theme = pkgs.stdenv.mkDerivation {
+                    pname = "distro-grub-themes";
+                    version = "3.1";
+                    src = pkgs.fetchFromGitHub {
+                        owner = "AdisonCavani";
+                        repo = "distro-grub-themes";
+                        rev = "v3.1";
+                        hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+                    };
+                    installPhase = "cp -r customize/nixos $out";
+                };
+                #theme = (pkgs.sleek-grub-theme.override {
+                #    withBanner = "Grub Bootloader";
+                #    withStyle = "orange";
+                #});
+    	    };
+        };
 
-    boot.loader = {
-        systemd-boot.enable = false;
-	    efi = {
-	        canTouchEfiVariables = true;
-    	    efiSysMountPoint = "/boot";
-    	};
-    	grub = {
-    	    devices = [ "nodev" ];
-    	    enable = true;
-    	    efiSupport = true;
-    	    useOSProber = false;
-    	};
-    };
-
-    boot.initrd.systemd.enable = true; 
-    boot.kernelParams = [
-        "quiet" 
-        "splash" 
-        "vga=current"
-        "rd.systemd.show_status=false"
-        "rd.udev.log_level=3"
-        "udev.log_priority=3"
-    ];
-    boot.consoleLogLevel = 0;
-    boot.initrd.verbose = false;
-    boot.plymouth = {
-        enable = true;
-        themePackages = [ pkgs.adi1090x-plymouth ];
-        theme = "dark_planet";
-        font = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNLNerdFont-Regular.ttf";
+        initrd.systemd.enable = true; 
+        kernelParams = [
+            "quiet" 
+            "splash" 
+            "vga=current"
+            "rd.systemd.show_status=false"
+            "rd.udev.log_level=3"
+            "udev.log_priority=3"
+        ];
+        consoleLogLevel = 0;
+        initrd.verbose = false;
+        plymouth = {
+            enable = true;
+            themePackages = [ pkgs.adi1090x-plymouth ];
+            theme = "dark_planet";
+            font = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNLNerdFont-Regular.ttf";
+        };
     };
 }
