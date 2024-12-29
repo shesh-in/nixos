0e20aef7c12525b061bdb8dcf12aaaa36a672999
shesh-in <6970317@gmail.com>
2024-12-29 13:24:06 +0300

    switched to systemdboot

---
+++ options/boot.nix
@@ -0,0 +1,57 @@
+{ config, lib, pkgs, ... }: {
+
+    environment.systemPackages = with pkgs; [
+        #grub2
+        #os-prober
+        efibootmgr
+    ];
+    boot = {
+        loader = {
+            timeout = 0;
+            systemd-boot.enable = true;
+	        efi = {
+	            canTouchEfiVariables = true;
+    	        efiSysMountPoint = "/boot";
+    	    };
+    	    #grub = {
+    	    #    devices = [ "nodev" ];
+    	    #    enable = true;
+    	    #    efiSupport = true;
+    	    #    useOSProber = false;
+            #    timeoutStyle = "hidden";
+            #    theme = pkgs.stdenv.mkDerivation {
+            #        pname = "distro-grub-themes";
+            #        version = "3.1";
+            #        src = pkgs.fetchFromGitHub {
+            #            owner = "AdisonCavani";
+            #            repo = "distro-grub-themes";
+            #            rev = "v3.1";
+            #            hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+            #        };
+            #        installPhase = "cp -r customize/nixos $out";
+            #    };
+            #    extraConfig = ''
+            #        set timeout = 0
+            #    '';
+    	    #};
+        };
+
+        initrd.systemd.enable = true; 
+        kernelParams = [
+            "quiet" 
+            "splash" 
+            "vga=current"
+            "rd.systemd.show_status=false"
+            "rd.udev.log_level=3"
+            "udev.log_priority=3"
+        ];
+        consoleLogLevel = 0;
+        initrd.verbose = false;
+        plymouth = {
+            enable = true;
+            themePackages = [ pkgs.adi1090x-plymouth ];
+            theme = "dark_planet";
+            font = "${pkgs.nerd-fonts.jetbrains-mono}/share/fonts/truetype/NerdFonts/JetBrainsMono/JetBrainsMonoNLNerdFont-Regular.ttf";
+        };
+    };
+}
