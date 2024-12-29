26f14840cf575286c2ffadcfdcd8654e0df8b45e
shesh-in <6970317@gmail.com>
2024-12-29 15:39:45 +0300

    shell aliases

--- options/aliases.nix
+++ options/aliases.nix
@@ -1,9 +1,10 @@
 { config, lib, pkgs, ...}:
 {
     programs.zsh.shellAliases = {
-        _ = "sudo";
-        l = "ls -alh --color = tty";
-        ll = "las -l";
+        "_" = "sudo";
+        l = "ls -alh --color=tty";
+        ll = "ls -l";
         grep = "grep --color=auto";
+        rm = "echo \"This is not the command you are looking for.\"; false";
     };
 }
